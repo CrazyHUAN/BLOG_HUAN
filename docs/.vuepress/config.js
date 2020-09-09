182d33178e8badeaebcd9f9a052c5a16660d8953
CrazyHUAN <cunhuangogogo@126.com>
2020-09-09 19:05:52 +0800

    更新个人简介

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,8 +8,8 @@ module.exports = {
   base: process.argv.splice(-1)[0],
 
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
-    ['meta', { name: 'keywords', content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'}],
+    ['link', { rel: 'icon', href: '/img/favicon11.ico' }], //favicons，资源放在public文件夹
+    ['meta', { name: 'keywords', content: '博客,个人技术博客,前端,前端开发,算法,计算机视觉,SLAM,3D建模,OpenCV,后台,工具文档,技术文档,学习,面试,JavaScript,js,ES6,vue,python,git,cmake,github,markdown'}],
     ['meta', { name: 'baidu-site-verification', content: 'uMIWIzKJNL'}],// 百度统计的站点拥有者验证
     ['meta', { name: 'theme-color', content: '#11a8cd'}], // 移动浏览器主题颜色
     // ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'}], // 移动端阻止页面缩放
@@ -24,8 +24,7 @@ module.exports = {
   themeConfig: { // 主题配置
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo_huan.png', // 导航栏logo
-    repo: 'CrazyHUAN/BLOG_HUAN', // 导航栏右侧生成Github链接
+    logo: '/img/logo1_huan.png', // 导航栏logo
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
